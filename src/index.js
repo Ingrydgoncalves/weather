bc7f5639eca9d45f07840ee4470412282dc36f50
Ingrydgoncalves <ingryd-lorrene@hotmail.com>
2025-05-04 17:00:53 -0300

    changed html in java script

--- src/index.js
+++ src/index.js
@@ -67,3 +67,33 @@ let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+
+
+function displayforecast () {
+  let days= [ "Tue","Wed","Thu","Fri","Sat"];
+  let forecastHtml= "";
+  days.forEach(function (day) {
+    forecastHtml= 
+    forecastHtml + `
+ 
+ <div class="weather-forecast-day">
+   <div class="weather-forecast-date"> ${day} </div>
+   <div class="weather-forecast-icon">☀️</div>
+   <div class="weather-forecast-temperatures">
+     <div class="weather-forecast-temperature">
+     <strong> 15°</strong>
+   </div>
+   <div class="weather-forecast-temperature"> 9°</div>
+ </div> 
+ </div>
+ `;
+  });
+  let forecastElement= document.querySelector("#forecast");
+   forecastElement.innerHTML= forecastHtml;
+}
+
+function getForecast (city) {
+   let apiKey = "ce024dff2t11ac0410oc03803af4bc0a";
+   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+   
+}
