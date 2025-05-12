9c46add12fd2cff3faec235bbdefdc981baffe72
Ingrydgoncalves <ingryd-lorrene@hotmail.com>
2025-05-12 19:03:08 -0300

    getForecast for city

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ function search(event) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
-  getForecast("Paris");
+  getForecast(city);
 }
 
 function formatDate(date) {
